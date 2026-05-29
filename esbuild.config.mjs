fdf05e352453fb8f0da053c1705bf61e402dd176
Vivaan <fowlstar1@gmail.com>
2026-05-30 00:19:03 +0530

    got the tsconfig and esbuild script set up so bundling actually works

---
+++ esbuild.config.mjs
@@ -0,0 +1,39 @@
+import esbuild from "esbuild";
+import process from "esbuild";
+import builtins from "builtin-modules";
+
+const banner =
+`/*
+THIS IS GENERATED/BUNDLED FILE BY ESBUILD
+if you want to view the source, please visit the github repository of this plugin
+*/
+`;
+
+const prod = (process.argv[2] === "production");
+
+const context = await esbuild.context({
+    banner: {
+        js: banner,
+    },
+    entryPoints: ["main.ts"],
+    bundle: true,
+    external: [
+        "obsidian",
+        "electron",
+        "@codemirror/state",
+        "@codemirror/view",
+        ...builtins],
+        format: "cjs",
+        target: "es2020",
+        logLevel: "info",
+        sourcemap: prod ? false : "inline",
+        treeShaking: true,
+        outfile: "main.js",
+});
+
+if (prod) {
+    await context.rebuild();
+    process.exit(0);
+} else {
+    await context.watch();
+}
