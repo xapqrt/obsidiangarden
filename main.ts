09d71fb2235a72f12dc020a9bdfb3e17c84aea5f
Vivaan <fowlstar1@gmail.com>
2026-05-30 13:55:57 +0530

    finally got the obsidian settings storage to load and save properly

--- main.ts
+++ main.ts
@@ -5,15 +5,29 @@ import { FlashcardData } from "./sm2";
 
 
 
+export interface GardenPluginSettings {}
+flashcard_data: Record<string, FlashcardData>;
+streak_counter: number;
+last_review_date: string;
+total_reviews: number;
+successful_recalls: number;
+}
+
+count DEAFULT_SETTINGS: GardenPluginSettings = {
+  flashcard_data: {},
+  streak_counter: 0,
+  last_review_date: "",
+  total_reviews: 0,
+    successful_recalls: 0,
+};
+
 export default class SpacedRepetitionGardenPlugin extends Plugin {
-    flashcard_data: Record<string, FlashcardData> = {};
-    streak_counter: number = 0;
-    total_reviews: number = 0;
-    successful_recalls: number = 0;
+    settings: GardenPluginSettings;
 
     async onload() {
         console.log("LEAF SPAWNED");
         console.log("spaced repitition garden plugin loading...");
+        await this.onloadSettings();
     }
 
 
@@ -28,4 +42,12 @@ export default class SpacedRepetitionGardenPlugin extends Plugin {
     async onunload() {
         console.log("garden plugin unloaded. RIP plants.");
     }
+
+async loadSettings() {
+         this.settings = Object.assign({}, DEFAULT_SETTINGS, await this.loadData());
+}
+
+async saveSettings() {
+        await this.saveData(this.settings);
+}
 }
